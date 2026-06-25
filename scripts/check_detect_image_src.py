e19978bafc6bcbc187d14b6ba1b876b2bf035fd3
Juan Manuel Iriondo Ortega <54706910+jumair@users.noreply.github.com>
2026-06-25 14:00:30 +0200

    feat: prepared file check_detect_image_src.py, #6

--- scripts/check_detect_image_src.py
+++ scripts/check_detect_image_src.py
@@ -9,7 +9,15 @@ from src.detect_image import detect_image
 
 if __name__ == "__main__":
 
-    img = Path("data/demo/cocacola1.jpg")
+    # Para llamar a la función detect_image que está en src hay que pasarle la imagen en modo path
+    # y el modelo (que está en models/best.pt) en modo path
+    # y como resultado en el directorio data/outputs devuelve un fichero annoted_pepsi_cocacola1.jpg (annoted_nombre_fichero_imagen.jpg)
+
+    # Como llamar desde terminal a detect_image.py de src -> python -m src.detect_image --image data/demo/pepsi_cocacola1.jpg
+
+    # Para el ejemplo de llamada la imagen pepsi_cocacola1.jpg debe estar en la carpeta data/demo/
+
+    img = Path("data/demo/pepsi_cocacola1.jpg")
     model = Path("models/best.pt")
      
     out = detect_image(img, model)
